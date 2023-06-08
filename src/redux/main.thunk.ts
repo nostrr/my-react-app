063d24b4d9495fad98df71cef5854c6762abf852
Oleksii Lukavnov <lukavnev@gmail.com>
2023-06-08 21:55:03 +0300

    Deleted folder redux

--- src/redux/main.thunk.ts
+++
@@ -1,7 +0,0 @@
-import {Email, MailData} from '../mailApi';
-import {createAsyncThunk} from "@reduxjs/toolkit";
-
-export const sendEmail = createAsyncThunk(
-    'main/sendEmail',
-    async (mailBody:string) => await Email.send({...MailData, Body: mailBody})
-);
